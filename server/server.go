45461773427188f3488fbe41aade48a985c30bfb
Max <mag030@ucsd.edu>
2018-11-17 15:15:53 -0800

    Modified the structs to link projects by email. Started on the CreateProjectRequest but have not tested. Need to implement FetchProjectRequest

--- server/server.go
+++ server/server.go
@@ -30,6 +30,14 @@ func main() {
 
 	DB = &mongo{m.DB("tea").C("projects")} //change collection to projects
 
+    //Using email to find users projects in database. Won't allow duplicate emails.
+    DB.Operation.EnsureIndex(mgo.Index{
+        Key:        []string{"email"},
+        Unique:     true,
+        DropDups:   true,
+        Background: true,
+        Sparse:     true,})
+
 	// Host grpc server
 	listen, err := net.Listen("tcp", "127.0.0.1:50052")
 	if err != nil {
@@ -47,43 +55,43 @@ func main() {
 
 func (s *server) CreateProject(ctx context.Context, crProjReq *pb.CreateProjectRequest) (*pb.CreateProjectResponse, error) {
 
-	//first check if this project has already been created
-	project := &pb.CreateProjectRequest{}
-	err1 := DB.Operation.Find(
-		bson.M{
-			"title":      crProjReq.Title,
-			"descripton": crProjReq.Description,
-			"deadline":   crProjReq.Deadline,
-			"private":    crProjReq.Deadline,
-		}).One(project)
+    //Get user projects
+	projects := &pb.UserProjects{}
+	err1 := DB.Operation.Find(bson.M{ "email": crProjReq.Email}).One(projects)
 
+    //User projects not found
 	if err1 != nil {
-		return &pb.CreateProjectResponse{Success: false}, err1
+		//Create user projects
+        projects.Email = crProjReq.Email
+        DB.Operation.Insert(projects.Email)
 	}
 
 	//if a project was already found, then we can't create this project
-	if (&pb.CreateProjectRequest{}) != project {
-		return &pb.CreateProjectResponse{Success: false}, err1
+	if  projects.Projects[crProjReq.Title] != nil {
+		return &pb.CreateProjectResponse{Success: false}, nil
 	}
 
 	//otherwise, go ahead and create the project
-	err := DB.Operation.Insert(
-		bson.M{
-			"title":          crProjReq.Title,
-			"Users":          [1]string{crProjReq.Email},
-			"descripton":     crProjReq.Description,
-			"deadline":       crProjReq.Deadline,
-			"private":        crProjReq.Private,
-			"Announcements":  []string{},
-			"size":           1,
-			"project_leader": crProjReq.Email,
-			"progress_bar":   0,
-			"done":           false,
-			"Milestones":     []string{},
-		})
+	projects.Projects[crProjReq.Title] = &pb.Project{
+			Title:          crProjReq.Title,
+			Users:          []string{crProjReq.Email},
+			Description:    crProjReq.Description,
+			Deadline:       crProjReq.Deadline,
+			Private:        crProjReq.Private,
+			Announcements:  []string{},
+			Size:           1,
+			ProjectLeader:  crProjReq.Email,
+			ProgressBar:    0,
+			Done:           false,
+			Milestones:     []string{}}
+
+    //Insert to database
+    err := DB.Operation.Update( bson.M{ "email": crProjReq.Email}, projects )
+
 	if err != nil {
 		return &pb.CreateProjectResponse{Success: false}, err
 	}
+
 	return &pb.CreateProjectResponse{Success: true}, nil
 
 }
