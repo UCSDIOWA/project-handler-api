9c61783bf9129999047a4ec92e53c118385e4f95
Manik Narang <narang_manik@rocketmail.com>
2018-11-25 23:56:49 -0800

    Prepare to deploy

--- server.go
+++ server.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 	"net"
+	"net/http"
+	"os"
 
 	pb "github.com/UCSDIOWA/project-handler-api/protos"
 	"github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
 )
 
@@ -19,24 +24,47 @@ type mongo struct {
 
 // Host mongo server. Updating scope of of m
 var (
-	m, err = mgo.Dial("mongodb://tea:cse110IOWA@ds159263.mlab.com:59263/tea")
+	m, err       = mgo.Dial("mongodb://tea:cse110IOWA@ds159263.mlab.com:59263/tea")
+	echoEndpoint = flag.String("echo_endpoint", "localhost:50052", "endpoint of project-handler-api")
 )
 
 // DB is a pointer to mongo struct
 var DB *mongo
 
 func main() {
+	errors := make(chan error)
 
+	go func() {
+		errors <- startGRPC()
+	}()
+
+	go func() {
+		flag.Parse()
+		defer glog.Flush()
+
+		errors <- startHTTP()
+	}()
+
+	for err := range errors {
+		log.Fatal(err)
+		return
+	}
+}
+
+func startGRPC() error {
+	// Host mongo server
+	m, err := mgo.Dial("mongodb://tea:cse110IOWA@ds159263.mlab.com:59263/tea")
 	if err != nil {
 		log.Fatalf("Could not connect to the MongoDB server: %v", err)
 	}
 	defer m.Close()
+	log.Println("Connected to MongoDB server")
 
-	DB = &mongo{m.DB("tea").C("projects")} //change collection to projects
-
-	//Using email to find users projects in database. Won't allow duplicate emails.
+	// Accessing user collection in tea database
+	DB = &mongo{m.DB("tea").C("projects")}
+	// Using email to find users in the database. Won't allow duplicated emails.
 	DB.Operation.EnsureIndex(mgo.Index{
-		Key:        []string{"ProjectID"},
+		Key:        []string{"email"},
 		Unique:     true,
 		DropDups:   true,
 		Background: true,
@@ -55,6 +83,29 @@ func main() {
 	if err := s.Serve(listen); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
+
+	return err
+}
+
+func startHTTP() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := pb.RegisterProjectCreatorAPIHandlerFromEndpoint(ctx, mux, *echoEndpoint, opts)
+	if err != nil {
+		return err
+	}
+	log.Println("Listening on port 8080")
+
+	herokuPort := os.Getenv("PORT")
+	if herokuPort == "" {
+		herokuPort = "8080"
+	}
+
+	return http.ListenAndServe(":"+herokuPort, mux)
 }
 
 /* This function creates a project for a given user */
